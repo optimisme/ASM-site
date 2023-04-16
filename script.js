835bc4e0c246fdba5ad48a8a687c7a658ba51c1a
optimisme <optimisme@gmail.com>
2023-04-16 13:20:13 +0200

    Primera versió de prova

---
+++ script.js
@@ -0,0 +1,54 @@
+
+var header = null;
+var carousel = null;
+var slides = null;
+var prevButton = null;
+var nextButton = null;
+var currentSlide = 0;
+
+window.addEventListener("load", init)
+function init () {
+
+    header = document.querySelector('#header');
+
+    carousel = document.querySelector('.about-us-carousel');
+    slides = Array.from(document.querySelectorAll('.about-us-slide'));
+    prevButton = document.querySelector('.carousel-button-prev');
+    nextButton = document.querySelector('.carousel-button-next');
+
+    window.addEventListener("scroll", scrollHandler);
+
+    initLinksTopPosition();
+}
+
+function scrollHandler () {
+    let diff = parseInt(window.pageYOffset - header.offsetTop);
+    if (diff === 0) {
+        console.log("shadow")
+        header.classList.add('shadow');
+    } else {
+        console.log("no shadow")
+        header.classList.remove('shadow');
+    }
+}
+
+function initLinksTopPosition () {
+
+    let links = document.querySelectorAll('a[href^="#"]')
+
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault()
+
+            var targetId = link.getAttribute('href')
+            var target = document.querySelector(targetId)
+            var menuHeight = header.offsetHeight
+
+            if (target) {
+                var targetPosition = target.offsetTop - menuHeight;
+                window.scrollTo({ top: targetPosition, behavior: 'smooth' })
+            }
+        })
+    })
+
+}

44764c327c75e0b79ac58263cb2885a0f5a258bb
optimisme <optimisme@gmail.com>
2023-04-20 14:07:27 +0200

    New version with AEF

---
+++ AEF/script.js
@@ -0,0 +1,41 @@
+
+var header = null;
+var carousel = null;
+var slides = null;
+var prevButton = null;
+var nextButton = null;
+var currentSlide = 0;
+
+window.addEventListener("load", init)
+function init () {
+
+    header = document.querySelector('#header');
+
+    carousel = document.querySelector('.about-us-carousel');
+    slides = Array.from(document.querySelectorAll('.about-us-slide'));
+    prevButton = document.querySelector('.carousel-button-prev');
+    nextButton = document.querySelector('.carousel-button-next');
+
+    initLinksTopPosition();
+}
+
+function initLinksTopPosition () {
+
+    let links = document.querySelectorAll('a[href^="#"]')
+
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault()
+
+            var targetId = link.getAttribute('href')
+            var target = document.querySelector(targetId)
+            var menuHeight = header.offsetHeight
+
+            if (target) {
+                var targetPosition = target.offsetTop - menuHeight;
+                window.scrollTo({ top: targetPosition, behavior: 'smooth' })
+            }
+        })
+    })
+
+}
